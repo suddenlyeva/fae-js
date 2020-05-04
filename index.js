313f4f53b5fe74b44f9bba3b66972bae1182355e
Eva Wong <suddenlyeva@gmail.com>
2020-05-04 13:27:55 -0700

    typecasting and fix env setup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("json-circular-stringify");
 var src = fs.readFileSync('test.fae', 'utf8')
 
 function isLetter(c) {
-  return c.toLowerCase() != c.toUpperCase();
+  return c.toLowerCase() != c.toUpperCase()
 }
 
 /////////////////
@@ -588,20 +588,33 @@ while (cur < src.length) {
   else if (src[cur] == '"') {
     let symbol = src[cur]
     let lineinfo = `${line}:${col}`
-    let escape = false
     next()
-    while(src[cur] != '"' || escape) {
-      if (!src[cur]) {
+    while(src[cur] != '"') {
+      if (!src[cur] || src[cur] == '\n' ) {
         throw Error(`At Line ${lineinfo} - End of string not found`)
       }
-      if (src[cur] == '\\' && !escape) {
-        escape = true
+      if (src[cur] == '\\') {
+        next()
+        if (src[cur] == '"') {
+          symbol += src[cur]
+          next()
+        }
+        else if (src[cur] == 'n') {
+          symbol += '\n'
+          next()
+        }
+        else if (src[cur] == 't') {
+          symbol += '\t'
+          next()
+        }
+        else {
+          throw Error(`At Line ${lineinfo} - Invalid escape character.`)
+        }
       }
       else {
-        escape = false
+        symbol += src[cur]
+        next()
       }
-      symbol += src[cur]
-      next()
     }
     next()
     symbol += src[cur]
@@ -695,22 +708,12 @@ function ParserError(token, message) {
   return `ParserError at Line ${token.line} - ${message}`
 }
 
-function ParseEnvironment({tokens,parent,args}) {
+function ParseEnvironment({tokens,args}) {
 
   let environment = {}
+  let vars = args
 
-  let vars = {}
-
-  if (args) {
-    for (let arg of args) {
-      vars[arg] = { type: '<Undefined>' , value: null }
-    }
-  }
-
-  environment.scope = { vars }
-  if (parent) {
-    environment.scope.parent = parent.scope
-  }
+  environment.vars = vars
   environment.statements = []
 
   //
@@ -785,10 +788,16 @@ function ParseEnvironment({tokens,parent,args}) {
         ++t
       }
       let func_tokens = tokens.slice(block_start, t)
+      let passed_args = {}
+      for (let arg of func.args) {
+        passed_args[arg] = {
+          type: '<Undefined>',
+          value: null
+        }
+      }
       func.body = ParseEnvironment({
         tokens: func_tokens, 
-        parent: environment, 
-        args: func.args
+        args: passed_args
       })
       if (type == 'TASK') {
         func.body.running = {
@@ -857,7 +866,7 @@ function ParseEnvironment({tokens,parent,args}) {
         }
         tokens.shift()
       }
-      loop.body = ParseBlock({ tokens, parent: environment })
+      loop.body = ParseBlock({tokens})
       environment.statements.push(loop)
     }
 
@@ -878,7 +887,7 @@ function ParseEnvironment({tokens,parent,args}) {
       }
       tokens.shift()
 
-      statement.body = ParseBlock({ tokens, parent: environment })
+      statement.body = ParseBlock({tokens})
       environment.statements.push(statement)
     }
 
@@ -912,7 +921,7 @@ function ParseEnvironment({tokens,parent,args}) {
         statement.statement = 'ELSE'
       }
 
-      statement.body = ParseBlock({ tokens, parent: environment })
+      statement.body = ParseBlock({tokens})
       top.else = statement
     }
 
@@ -944,7 +953,13 @@ function ParseEnvironment({tokens,parent,args}) {
       }
       tokens.shift()
 
-      statement.body = ParseBlock({ tokens, parent: environment, args: [statement.variable] })
+      let args = {}
+      args[statement.variable] = {
+        type: '<Undefined>',
+        value: null
+      }
+
+      statement.body = ParseBlock({ tokens, args })
       environment.statements.push(statement)
     }
 
@@ -1139,7 +1154,7 @@ function ParseTerm({tokens}) {
 
 function ParseSuffix({tokens}) {
   let left = ParseTerm({tokens})
-  while(tokens[0].symbol == '.' || tokens[0].symbol == '(' || tokens[0].symbol == '[') {
+  while(tokens[0].symbol == '.' || tokens[0].symbol == '(' || tokens[0].symbol == '[' || tokens[0].symbol == '<') {
     let operation = tokens[0].symbol
     let line = tokens[0].line
 
@@ -1207,6 +1222,23 @@ function ParseSuffix({tokens}) {
         args
       }
       left = op
+    }
+    // Typecast
+    else if (operation == '<') {
+      if (tokens[1].type == 'IDENTIFIER' 
+      && tokens[2] && (tokens[2].symbol == '>' || tokens[2].symbol == ',')) {
+        operation = '<>'
+        let single = tokens[2].symbol == '>'
+        let right = ParseTerm({tokens})
+        let op = {
+          line,
+          left,
+          operation,
+          single,
+          right
+        }
+        left = op
+      }
     }
   }
   return left
@@ -1299,9 +1331,9 @@ function ParseUnion({tokens}) {
   return left
 }
 
-function ParseTypeOf({tokens}) {
+function ParseRange({tokens}) {
   let left = ParseUnion({tokens})
-  while(tokens[0].symbol == '&') {
+  while(tokens[0].symbol == '..') {
     let operation = tokens[0].symbol
     let line = tokens[0].line
     tokens.shift()
@@ -1316,26 +1348,9 @@ function ParseTypeOf({tokens}) {
   return left
 }
 
-function ParseRange({tokens}) {
-  let left = ParseTypeOf({tokens})
-  while(tokens[0].symbol == '..') {
-    let operation = tokens[0].symbol
-    let line = tokens[0].line
-    tokens.shift()
-    let op = {
-      line,
-      left,
-      operation,
-      right : ParseTypeOf({tokens})
-    }
-    left = op
-  }
-  return left
-}
-
 function ParseComparison({tokens}) {
   let left = ParseRange({tokens})
-  while(tokens[0].symbol == '<' || tokens[0].symbol == '>' || tokens[0].symbol == '<=' || tokens[0].symbol == '>=') {
+  if(tokens[0].symbol == '<' || tokens[0].symbol == '>' || tokens[0].symbol == '<=' || tokens[0].symbol == '>=') {
     let operation = tokens[0].symbol
     let line = tokens[0].line
     tokens.shift()
@@ -1352,7 +1367,7 @@ function ParseComparison({tokens}) {
 
 function ParseEquality({tokens}) {
   let left = ParseComparison({tokens})
-  while(tokens[0].symbol == '==' || tokens[0].symbol == '!=') {
+  if(tokens[0].symbol == '==' || tokens[0].symbol == '!=' || tokens[0].symbol == '&' ) {
     let operation = tokens[0].symbol
     let line = tokens[0].line
     tokens.shift()
@@ -1463,12 +1478,12 @@ function ParseStatement({tokens}) {
   }
 }
 
-function ParseBlock({tokens,parent,args}) {
+function ParseBlock({tokens,args}) {
   if (tokens[0].symbol != '{') {
     throw ParserError(tokens[0], `Expected '{' at start of code block`)
   }
   tokens.shift()
-  let env = ParseEnvironment({tokens,parent,args})
+  let env = ParseEnvironment({tokens,args})
   if (tokens[0].symbol != '}') {
     throw ParserError(tokens[0], `Expected '}' at end of code block`)
   }
@@ -1480,7 +1495,7 @@ function ParseBlock({tokens,parent,args}) {
 //  S T A N D A R D   L I B R A R Y  //
 ///////////////////////////////////////
 
-let std = { scope: { vars: {
+let std = {
   print: {
     native(thing) {
       if (thing.type == '<Object>') {
@@ -1492,9 +1507,13 @@ let std = { scope: { vars: {
       return { type: '<Void>' }
     }
   },
-}}}
+  Object: {
+    type: '<Object>',
+    value: {}
+  }
+}
 
-std.scope.vars['<Number>'] = {
+std['<Number>'] = {
   String: {
     native(number) {
       return {
@@ -1513,7 +1532,7 @@ std.scope.vars['<Number>'] = {
   }
 }
 
-std.scope.vars['<String>'] = {
+std['<String>'] = {
   Boolean: {
     native(string) {
       return {
@@ -1521,10 +1540,15 @@ std.scope.vars['<String>'] = {
         value: string.value !== ''
       }
     }
-  }
+  },
+  String: {
+    native(string) {
+      return string
+    }
+  },
 }
 
-std.scope.vars['<Array>'] = {
+std['<Array>'] = {
   Boolean: {
     native(array) {
       return {
@@ -1532,10 +1556,18 @@ std.scope.vars['<Array>'] = {
         value: array.value.length !== 0
       }
     }
+  },
+  contains: {
+    native(array, item) {
+      return {
+        type: '<Boolean>',
+        value: !!array.value.find(i => i.value === item.value)
+      }
+    }
   }
 }
 
-std.scope.vars['<Object>'] = {
+std['<Object>'] = {
   Boolean: {
     native(object) {
       return {
@@ -1543,12 +1575,46 @@ std.scope.vars['<Object>'] = {
         value: Object.keys(object.value).length !== 0
       }
     }
+  },
+  String: {
+    native(object) {
+      return {
+        type: '<String>',
+        value: JSON.stringify(object, null, 2)
+      }
+    }
+  },
+  keys: {
+    native(object) {
+      let keys = []
+      for (let key of Object.keys(object.value)) {
+        keys.push({
+          type: '<String>',
+          value: key
+        })
+      }
+      return {
+        type: '<Array>',
+        value: keys
+      }
+    }
   }
+}
+
+std['<*>'] = {
+  type: {
+    native(any) {
+      return {
+        type: '<String>',
+        value: any.type
+      }
+    }
+  },
 }
 
 let AST
 try {
-  AST = ParseEnvironment({ tokens: token_list, parent: std })
+  AST = ParseEnvironment({ tokens: token_list, args: std })
   if (token_list[0].type != 'EOF') {
     throw ParserError(token_list[0], `Invalid statement`)
   }
@@ -1566,39 +1632,44 @@ let env = AST
 function create_stack(env) {
   let stack = []
   
-  stack.push_environment = (env, args = {}) => {
-    let statements = []
-    for (let statement of env.statements) {
-      let new_statement = {}
-      for (let key of Object.keys(statement)) {
-        if (key == 'body') {
-          new_statement.body = statement.body
-        }
-        else {
-          new_statement[key] = JSON.parse(JSON.origStringify(statement[key]))
-        }
-      }
-      statements.push(new_statement)
-    }
-    let new_env = {
-      scope : env.scope,
-      statements,
-      running : env.running
-    }
-    for (let key in env.scope.vars) {
-      if (env.scope.vars[key].hasOwnProperty('value')) {
-        new_env.scope.vars[key] = JSON.parse(JSON.origStringify(env.scope.vars[key]))
-      }
-      else {
-        new_env.scope.vars[key] = env.scope.vars[key]
-      }
-    }
+  stack.push_environment = (pushed_env, args = {}) => {
+    // let statements = []
+    // for (let statement of env.statements) {
+    //   let new_statement = {}
+    //   for (let key of Object.keys(statement)) {
+    //     if (key == 'body' || key == 'else') {
+    //       new_statement[key] = statement[key]
+    //     }
+    //     else {
+    //       new_statement[key] = JSON.parse(JSON.origStringify(statement[key]))
+    //     }
+    //   }
+    //   statements.push(new_statement)
+    // }
+    // let new_env = {
+    //   scope : env.scope,
+    //   statements,
+    //   running : env.running
+    // }
+    // new_env.scope.vars = {}
+    // for (let key in env.scope.vars) {
+    //   if (env.scope.vars[key].hasOwnProperty('value')) {
+    //     new_env.scope.vars[key] = JSON.parse(JSON.origStringify(env.scope.vars[key]))
+    //   }
+    //   else {
+    //     new_env.scope.vars[key] = env.scope.vars[key]
+    //   }
+    // }
+    let new_env = JSON.parse(JSON.origStringify(pushed_env))
     for (let key in args) {
-      new_env.scope.vars[key] = args[key]
+      new_env.vars[key] = args[key]
     }
+    new_env.parent = env
+    env = new_env
     stack.push(new_env)
     return new_env
   }
+
   if (env) stack.push(env)
   return stack
 }
@@ -1644,9 +1715,29 @@ machine.advance = () => {
 function TypeError(line, message) {
   return `TypeError at Line ${line} - ${message}`
 }
+    
+function same_type(left, right) {
+  if (left.type != right.type) {
+    return false
+  }
+  if (left.type == '<Object>') {
+    if (Object.keys(right.value).length === 0) {
+      return true
+    }
+    for (let key in right.value) {
+      if (!left.value[key]) {
+        return false
+      }
+      if (!same_type(left.value[key],right.value[key])) {
+        return false
+      }
+    }
+  }
+  return true
+}
 
 function search(variable) {
-  let scope = env.scope
+  let scope = env
   while (scope != null) {
     if (scope.vars[variable]) {
       return scope.vars[variable]
@@ -1659,7 +1750,7 @@ function search(variable) {
 }
 
 function try_search(variable,{type,polytype}) {
-  let scope = env.scope
+  let scope = env
   while (scope != null) {
     if (type) {
       if (polytype) {
@@ -1744,7 +1835,7 @@ function interpret(stack) {
     if (top.eval.hasOwnProperty('value') && !top.eval.init) {
       if (top.eval.value) {
         stack.push_environment(top.body)
-        top.eval = null
+        delete top.eval
       }
       else {
         stack.pop()
@@ -1757,30 +1848,34 @@ function interpret(stack) {
 
   // For
   else if (top.statement == 'FOR') {
-    if (top.in.variable) {
-      top.in = search(top.in.variable)
+    if (top.eval == null) {
+      top.eval = JSON.parse(JSON.origStringify(top.in))
     }
-    if (!top.in.hasOwnProperty('value') || top.in.init) {
-      stack.push(top.in)
+    if (top.eval.variable) {
+      top.eval = search(top.eval.variable)
+    }
+    if (!top.eval.hasOwnProperty('value') || top.eval.init) {
+      stack.push(top.eval)
     }
     else if (top.at == null) {
       top.at = 0
     }
-    else if (top.at < top.in.value.length) {
+    else if (top.at < top.eval.value.length) {
       let args = {}
-      if (top.in.type == '<Array>') {
+      if (top.eval.type == '<Array>') {
         args[top.variable] = {
-          type: top.in.value[top.at].type,
-          value: top.in.value[top.at].value
+          type: top.eval.value[top.at].type,
+          value: top.eval.value[top.at].value
         }
       }
-      else if (top.in.type == '<String>') {
+      else if (top.eval.type == '<String>') {
         args[top.variable] = {
           type: '<String>',
-          value: top.in.value[top.at]
+          value: top.eval.value[top.at]
         }
       }
       stack.push_environment(top.body,args)
+      delete top.eval
       ++top.at
     }
     else {
@@ -2014,6 +2109,16 @@ function interpret(stack) {
   else if (top.operation == '.') {
     top.type  = '<Pointer>'
     top.value = top.left.value[top.right.value]
+    if (!top.value) {
+      top.value = {
+        type: '<Undefined>',
+        value: null,
+        set: {
+          key: top.right.value,
+          obj: top.left.value
+        }
+      }
+    }
     delete top.operation
     delete top.line
     delete top.left
@@ -2025,6 +2130,9 @@ function interpret(stack) {
   else if (top.operation == '.extension') {
     top.type  = '<Pointer>'
     top.value = try_search(top.right.value,top.left)
+    if (!top.value) {
+      throw TypeError(top.line, `Extension function "${top.right.value}" not found for ${top.left.type}.`)
+    }
     top.value.caller = top.left
     delete top.operation
     delete top.line
@@ -2113,6 +2221,7 @@ function interpret(stack) {
     else if (top.left.native != null) {
       if (top.left.caller) {
         top.args.unshift(top.left.caller)
+        delete top.left.caller
       }
       let ret = top.left.native(...top.args)
       top.type = ret.type
@@ -2129,6 +2238,7 @@ function interpret(stack) {
       let args = {}
       if (top.left.caller) {
         args.this = top.left.caller
+        delete top.left.caller
       }
       for (let i =  0; i < top.args.length && i < top.left.args.length; i++) {
         args[top.left.args[i]] = top.args[i]
@@ -2158,6 +2268,45 @@ function interpret(stack) {
       delete top.at
       delete top.fn
       stack.pop()
+    }
+  }
+
+  // Type Cast
+  else if (top.operation == '<>') {
+
+    if (same_type(top.left, top.right)) {
+      top.type = top.right.type
+      top.polytype = 
+        top.left.polytype && top.right.polytype ? top.left.polytype.concat(top.right.polytype) :
+        top.left.polytype || top.right.polytype
+      top.value = top.left.value
+      delete top.line
+      delete top.operation
+      delete top.single
+      delete top.right
+    }
+    else if (top.single) {
+
+      let type = top.right.polytype ? top.right.polytype[0] : top.right.type.slice(1,-1);
+
+      let conversion = try_search(type,top.left)
+      if (conversion) {
+        conversion.caller = top.left
+        top.operation = '()'
+        top.args = []
+        top.left = {
+          type: '<Pointer>',
+          value: conversion
+        }
+        delete top.right
+        delete top.single
+      }
+      else {
+        throw TypeError(top.line, `No extension found for converting ${top.left.type} to <${type}>.`)
+      }
+    }
+    else {
+      throw TypeError(top.line, `Object key mismatch in multitype cast operation.`)
     }
   }
 
@@ -2278,7 +2427,6 @@ function interpret(stack) {
     delete top.right
     stack.pop()
   }
-
   // Range
   else if (top.operation == '..') {
     top.type = '<Array>'
@@ -2374,6 +2522,20 @@ function interpret(stack) {
     stack.pop()
   }
 
+  // Type Check
+  else if (top.operation == '&') {
+    top.type  = '<Boolean>'
+    if (!top.left) {
+      throw top.line
+    }
+    top.value = same_type(top.left, top.right)
+    delete top.operation
+    delete top.line
+    delete top.left
+    delete top.right
+    stack.pop()
+  }
+
   // Stringer
   else if (top.operation == '~') {
     if (top.left.type == '<Array>' && top.right.type == '<Array>') {
@@ -2398,9 +2560,14 @@ function interpret(stack) {
 
   // Assign
   else if (top.operation == '=') {
-    top.left.type = top.right.type
-    top.left.polytype = top.right.polytype
-    top.left.value = top.right.value
+    if (top.left.set) {
+      top.left.set.obj[top.left.set.key] = top.right
+    }
+    else {
+      top.left.type = top.right.type
+      top.left.polytype = top.right.polytype
+      top.left.value = top.right.value
+    }
     delete top.operation
     delete top.line
     delete top.left
@@ -2489,18 +2656,45 @@ function interpret(stack) {
   else if (top.operation == '~=') {
     if (top.left.type == '<Array>' && top.right.type == '<Array>') {
       top.left.value = top.left.value.concat(top.right.value)
+      delete top.operation
+      delete top.line
+      delete top.left
+      delete top.right
+      stack.pop()
     }
     else if (top.left.type == '<Array>') {
       top.left.value.push(top.right)
+      delete top.operation
+      delete top.line
+      delete top.left
+      delete top.right
+      stack.pop()
     }
-    else if (top.left.value == '<String>') {
-      top.left.value += top.right.value
+    else if (top.left.type == '<String>') {
+      if (top.right.type == '<String>') {
+        top.left.value += top.right.value
+        delete top.operation
+        delete top.line
+        delete top.left
+        delete top.right
+        stack.pop()
+      }
+      else {
+        top.right = {
+          line: top.line,
+          left: top.right,
+          operation: '<>',
+          single: true,
+          right: {
+            type: '<String>',
+            value: ''
+          }
+        }
+      }
     }
-    delete top.operation
-    delete top.line
-    delete top.left
-    delete top.right
-    stack.pop()
+    else {
+      throw TypeError(top.line, `"~=" expected <Object> or <String>, found ${top.left.type}`)
+    }
   }
 
   // Optional Assign
@@ -2518,7 +2712,7 @@ function interpret(stack) {
   }
 
   // Set if false
-  else if (top.operation == '\=') {
+  else if (top.operation == '\\=') {
     if (!top.left.value) {
       top.left.type = top.right.type
       top.left.polytype = top.right.polytype
